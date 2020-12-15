d7240c224b55f3e4626685900c239e912a2116ae
Antoine-CJ-Thomas <antoine.cj.thomas@gmail.com>
2020-12-15 16:26:24 +0100

    Models created

---
+++ src/main/java/com/safetynet/alerts/model/Person.java
@@ -0,0 +1,79 @@
+package com.safetynet.alerts.model;
+
+public class Person {
+	
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String city;
+	private int zip;
+	private int phone;
+	private String email;
+
+	public Person(String firstName, String lastName, String address, String city, int zip, int phone, String email) {
+
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.city = city;
+		this.zip = zip;
+		this.phone = phone;
+		this.email = email;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public int getZip() {
+		return zip;
+	}
+
+	public void setZip(int zip) {
+		this.zip = zip;
+	}
+
+	public int getPhone() {
+		return phone;
+	}
+
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
