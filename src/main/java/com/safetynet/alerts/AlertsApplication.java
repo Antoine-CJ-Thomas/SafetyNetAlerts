5f5032b4db34e6cc51b67610e0eb10c23e2cf6a4
Antoine-CJ-Thomas <antoine.cj.thomas@gmail.com>
2020-12-28 16:42:09 +0100

    Service created in order to contain previous main method code

--- src/main/java/com/safetynet/alerts/AlertsApplication.java
+++ src/main/java/com/safetynet/alerts/AlertsApplication.java
@@ -3,33 +3,15 @@ package com.safetynet.alerts;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.safetynet.alerts.controller.FireStationController;
-import com.safetynet.alerts.controller.MedicalRecordController;
-import com.safetynet.alerts.controller.PersonController;
-import com.safetynet.alerts.dto.FireStationDTO;
-import com.safetynet.alerts.dto.MedicalRecordDTO;
-import com.safetynet.alerts.dto.PersonDTO;
-import com.safetynet.alerts.service.JsonFileReader;
+import com.safetynet.alerts.service.LaunchService;
 
 @SpringBootApplication
 public class AlertsApplication {
 
 	public static void main(String[] args) {
 
-		String filePath = "src/main/resources/data.json";
-
 		SpringApplication.run(AlertsApplication.class, args);
-
-		PersonDTO personDAO = new PersonDTO();
-		FireStationDTO fireStationDAO = new FireStationDTO();
-		MedicalRecordDTO medicalRecordDAO = new MedicalRecordDTO();
-
-		JsonFileReader jsonFileReader = new JsonFileReader(filePath, personDAO, fireStationDAO, medicalRecordDAO);
-
-		PersonController personController = new PersonController(personDAO);
-		FireStationController fireStationController = new FireStationController(fireStationDAO);
-		MedicalRecordController medicalRecordController = new MedicalRecordController(medicalRecordDAO);
-
-		jsonFileReader.readFile();
+		
+		LaunchService launchService = new LaunchService();
 	}
 }
